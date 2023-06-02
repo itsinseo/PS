bbe1d5a2e5e48a538ed8bc9288cc33de08afa761
Inseo Kim <itsinseo@gmail.com>
2023-06-02 12:19:55 +0900

    [level 0] Title: n의 배수 고르기, Time: 0.19 ms, Memory: 70.5 MB -BaekjoonHub

---
+++ 프로그래머스/lv0/120905. n의 배수 고르기/n의 배수 고르기.java
@@ -0,0 +1,18 @@
+import java.util.List;
+import java.util.LinkedList;
+
+class Solution {
+    public int[] solution(int n, int[] numlist) {
+        List<Integer> list = new LinkedList<>();
+        for(int i:numlist){
+            if(i%n==0){
+                list.add(i);
+            }
+        }
+        int[] answer = new int[list.size()];
+        for(int i=0; i< answer.length; i++){
+            answer[i] = list.get(i);
+        }
+        return answer;
+    }
+}
