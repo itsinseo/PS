8830e6afb68137d09e6a70d0ff901318806f840a
Inseo Kim <itsinseo@gmail.com>
2023-09-21 12:42:27 +0900

    [Silver V] Title: 영화감독 숌, Time: 300 ms, Memory: 86864 KB -BaekjoonHub

---
+++ 백준/Silver/1436. 영화감독 숌/영화감독 숌.java
@@ -0,0 +1,21 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+        int N = Integer.parseInt(br.readLine());
+
+        int count = 1;
+        int number = 666;
+
+        while (count < N) {
+            number++;
+            if (String.valueOf(number).contains("666")) count++;
+        }
+
+        System.out.println(number);
+    }
+}
