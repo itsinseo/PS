a589ac854e276c64d528aba03bc53a3c6e6c65e4
Inseo Kim <itsinseo@gmail.com>
2023-09-25 10:09:46 +0900

    [Silver II] Title: 약수의 합 2, Time: 140 ms, Memory: 14392 KB -BaekjoonHub

---
+++ 백준/Silver/17427. 약수의 합 2/약수의 합 2.java
@@ -0,0 +1,17 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+        int N = Integer.parseInt(br.readLine());
+
+        long g = 0;
+        for (int i = 1; i <= N; i++) {
+            g += i * (N / i);
+        }
+        System.out.println(g);
+    }
+}
