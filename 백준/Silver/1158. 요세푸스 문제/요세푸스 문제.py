ee9515f4e47099abaf3b8e1439e92abf40430f8a
Inseo Kim <itsinseo@gmail.com>
2023-10-16 08:59:56 +0900

    [Silver IV] Title: 요세푸스 문제, Time: 40 ms, Memory: 30616 KB -BaekjoonHub

---
+++ 백준/Silver/1158. 요세푸스 문제/요세푸스 문제.py
@@ -0,0 +1,17 @@
+#1158 요세푸스 문제, 수정
+
+n, k = map(int, input().split())
+result = []
+lst = []
+idx = 0
+
+for i in range(1, n+1):
+    lst.append(i)
+
+for _ in range(n):
+    idx += k - 1
+    if idx >= len(lst):
+        idx = idx % len(lst)
+    result.append(str(lst.pop(idx)))
+
+print(f'<{", ".join(result)[:]}>')
