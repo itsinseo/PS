4f2f08ef8eea7c84456b3c07f09c73dfafde5e36
Inseo Kim <itsinseo@gmail.com>
2023-11-05 19:35:56 +0900

    [Silver V] Title: 피보나치 수 4, Time: 260 ms, Memory: 24808 KB -BaekjoonHub

---
+++ 백준/Silver/10826. 피보나치 수 4/피보나치 수 4.java
@@ -0,0 +1,18 @@
+import java.math.BigInteger;
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+
+        BigInteger[] dp = new BigInteger[10001];
+        dp[0] = BigInteger.valueOf(0);
+        dp[1] = BigInteger.valueOf(1);
+        for (int i = 2; i <= n; i++) {
+            dp[i] = dp[i - 1].add(dp[i - 2]);
+        }
+
+        System.out.println(dp[n]);
+    }
+}
