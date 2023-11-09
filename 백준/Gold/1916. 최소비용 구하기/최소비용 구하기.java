f7955da5a462fd81f04c93e815d515d663438528
Inseo Kim <itsinseo@gmail.com>
2023-11-09 23:54:45 +0900

    [Gold V] Title: 최소비용 구하기, Time: 724 ms, Memory: 46320 KB -BaekjoonHub

---
+++ 백준/Gold/1916. 최소비용 구하기/최소비용 구하기.java
@@ -0,0 +1,55 @@
+import java.io.*;
+import java.util.*;
+
+public class Main {
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+        int n = Integer.parseInt(br.readLine());
+        int m = Integer.parseInt(br.readLine());
+
+        int[][] graph = new int[n + 1][n + 1];
+        for (int i = 1; i <= n; i++) {
+            Arrays.fill(graph[i], Integer.MAX_VALUE);
+        }
+        int[] time = new int[n + 1];
+        Arrays.fill(time, Integer.MAX_VALUE);
+        int[] checked = new int[n + 1];
+
+        StringTokenizer st;
+        for (int i = 0; i < m; i++) {
+            st = new StringTokenizer(br.readLine());
+            int start = Integer.parseInt(st.nextToken());
+            int end = Integer.parseInt(st.nextToken());
+            int cost = Integer.parseInt(st.nextToken());
+            graph[start][end] = Math.min(graph[start][end], cost);
+        }
+
+        st = new StringTokenizer(br.readLine());
+        int start = Integer.parseInt(st.nextToken());
+        int end = Integer.parseInt(st.nextToken());
+
+        time[start] = 0;
+        Queue<Integer> queue = new LinkedList<>();
+        queue.add(start);
+
+        while (!queue.isEmpty()) {
+            int point = queue.poll();
+            checked[point] = 1;
+
+            for (int i = 1; i <= n; i++) {
+                if (graph[point][i] != Integer.MAX_VALUE) {
+                    if (time[point] + graph[point][i] < time[i]) {
+                        time[i] = time[point] + graph[point][i];
+                        queue.add(i);
+                    } else if (checked[i] == 0) {
+                        queue.add(i);
+                    }
+                }
+            }
+        }
+
+        System.out.println(time[end]);
+    }
+}
